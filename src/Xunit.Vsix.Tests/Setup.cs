3da5d63416f1a7671f850aef4131d0141ed536d4
Daniel Cazzulino <daniel@cazzulino.com>
2017-11-24 19:00:53 -0300

    Add extra renderings for the setup API for exploration

--- src/Xunit.Vsix.Tests/Setup.cs
+++ src/Xunit.Vsix.Tests/Setup.cs
@@ -5,15 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Setup.Configuration;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Xunit
 {
     public class Setup
     {
         private const int REGDB_E_CLASSNOTREG = unchecked((int)0x80040154);
+        ITestOutputHelper output;
 
-        //[Fact]
-        public void when_enumerating_vs_then_retrieves_all_instances()
+        public Setup(ITestOutputHelper output) => this.output = output;
+
+        [InlineData("Microsoft.VisualStudio.Workload.VisualStudioExtension")]
+        [InlineData("Microsoft.VisualStudio.Component.Merq")]
+        [Theory(Skip = "Just testing the setup APIs")]
+        public void when_enumerating_vs_then_retrieves_all_instances(string prerequisite)
         {
             var query = new SetupConfiguration();
             var query2 = (ISetupConfiguration2)query;
@@ -31,25 +37,26 @@ namespace Xunit
             } while (fetched > 0);
 
             var vssdk = from instance in instances
-                        let state = PrintInstance(instance, helper).GetState()
+                        let state = instance.GetState()
                         where state == InstanceState.Complete &&
                         (state & InstanceState.Local) == InstanceState.Local &&
-                        instance.GetPackages().Any(package => package.GetId() == "Microsoft.VisualStudio.Workload.VisualStudioExtension")
+                        instance.GetPackages().Any(package => package.GetId() == prerequisite)
                         select PrintInstance(instance, helper);
 
             vssdk.ToList();
         }
 
-        static ISetupInstance2 PrintInstance(ISetupInstance2 instance, ISetupHelper helper)
+        ISetupInstance2 PrintInstance(ISetupInstance2 instance, ISetupHelper helper)
         {
             var state = instance.GetState();
-            Console.WriteLine($"InstanceId: {instance.GetInstanceId()} ({(state == InstanceState.Complete ? "Complete" : "Incomplete")})");
+            output.WriteLine(new string('=', 100));
+            output.WriteLine($"InstanceId: {instance.GetInstanceId()} ({(state == InstanceState.Complete ? "Complete" : "Incomplete")})");
 
             var installationVersion = instance.GetInstallationVersion();
             var version = helper.ParseVersion(installationVersion);
             var catalog = instance as ISetupInstanceCatalog;
 
-            Console.WriteLine($"InstallationVersion: {installationVersion} ({version})");
+            output.WriteLine($"InstallationVersion: {installationVersion} ({version})");
 
             if (catalog != null)
             {
@@ -57,47 +64,46 @@ namespace Xunit
                 if (value != null)
                 {
                     var semver = NuGet.Versioning.SemanticVersion.Parse(value);
-                    Console.WriteLine($"SemanticVersion: {semver.Major}.{semver.Minor}.{semver.Patch} (Normalized: {semver.ToNormalizedString()}, Full: {semver.ToFullString()})");
+                    output.WriteLine($"SemanticVersion: {semver.Major}.{semver.Minor}.{semver.Patch} (Normalized: {semver.ToNormalizedString()}, Full: {semver.ToFullString()})");
                 }
             }
 
             if ((state & InstanceState.Local) == InstanceState.Local)
             {
-                Console.WriteLine($"InstallationPath: {instance.GetInstallationPath()}");
+                output.WriteLine($"InstallationPath: {instance.GetInstallationPath()}");
             }
 
             if (catalog != null)
             {
-                Console.WriteLine($"IsPrerelease: {catalog.IsPrerelease()}");
+                output.WriteLine($"IsPrerelease: {catalog.IsPrerelease()}");
             }
 
             if ((state & InstanceState.Registered) == InstanceState.Registered)
             {
-                Console.WriteLine($"Product: {instance.GetProduct().GetId()}");
-                Console.WriteLine("Workloads:");
+                output.WriteLine($"Product: {instance.GetProduct().GetId()}");
 
-                PrintWorkloads(instance.GetPackages());
+                PrintPackages(instance.GetPackages());
             }
 
             var properties = instance.GetProperties();
             if (properties != null)
             {
-                Console.WriteLine("Custom properties:");
+                output.WriteLine("Custom properties:");
                 PrintProperties(properties);
             }
 
             properties = catalog?.GetCatalogInfo();
             if (properties != null)
             {
-                Console.WriteLine("Catalog properties:");
+                output.WriteLine("Catalog properties:");
                 PrintProperties(properties);
             }
 
-            Console.WriteLine();
+            output.WriteLine("");
             return instance;
         }
 
-        static void PrintProperties(ISetupPropertyStore store)
+        void PrintProperties(ISetupPropertyStore store)
         {
             var properties = from name in store.GetNames()
                              orderby name
@@ -105,11 +111,11 @@ namespace Xunit
 
             foreach (var prop in properties)
             {
-                Console.WriteLine($"    {prop.Name}: {prop.Value}");
+                output.WriteLine($"    {prop.Name}: {prop.Value}");
             }
         }
 
-        static void PrintWorkloads(ISetupPackageReference[] packages)
+        void PrintWorkloads(ISetupPackageReference[] packages)
         {
             var workloads = from package in packages
                             where string.Equals(package.GetType(), "Workload", StringComparison.OrdinalIgnoreCase)
@@ -118,7 +124,25 @@ namespace Xunit
 
             foreach (var workload in workloads)
             {
-                Console.WriteLine($"    {workload.GetId()}");
+                output.WriteLine($"    {workload.GetId()}");
+            }
+        }
+
+        void PrintPackages(ISetupPackageReference[] packages)
+        {
+            var groups = from package in packages
+                         orderby package.GetId()
+                         group package by package.GetType() into grouped
+                         orderby grouped.Key
+                         select grouped;
+
+            foreach (var group in groups)
+            {
+                output.WriteLine($"{group.Key}:");
+                foreach (var package in group)
+                {
+                    output.WriteLine($"    {package.GetId()}");
+                }
             }
         }
     }

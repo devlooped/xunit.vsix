fdd4e7a20d4a2d94c0efe077a8f73e6a719c9cf8
Daniel Cazzulino <daniel@cazzulino.com>
2019-04-23 01:18:56 -0300

    Add support for limiting min/max version range for all tests
    This allows limiting in a single place, at the assembly level, what's the minimum supported version of VS, as well as what's the maximun supported version

--- src/Xunit.Vsix/XunitExtensions.cs
+++ src/Xunit.Vsix/XunitExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit.Abstractions;
 using Xunit.Sdk;
@@ -18,8 +19,14 @@ namespace Xunit
         public static IVsixAttribute GetVsixAttribute(this ITestMethod testMethod, IAttributeInfo vsixAttribute)
         {
             var vsVersions = testMethod.GetComputedProperty<string[]>(vsixAttribute, nameof(IVsixAttribute.VisualStudioVersions));
-            var minVersion = testMethod.GetComputedProperty<string>(vsixAttribute, nameof(IVsixAttribute.MinimumVisualStudioVersion));
-            var maxVersion = testMethod.GetComputedProperty<string>(vsixAttribute, nameof(IVsixAttribute.MaximumVisualStudioVersion));
+            // Higher versions always win for MinVersion, since this way we can toggle a minimum version 
+            // at the assembly level and override the per-test min version that might be obsolete. It's like 
+            // limiting the lower bound of what tests can override
+            var minVersion = testMethod.GetComputedMaxProperty<string>(vsixAttribute, nameof(IVsixAttribute.MinimumVisualStudioVersion));
+            // Conversely, for max verison we want the opposite, for the upper bound of supportability, we want 
+            // to be able to limit it to lower than a certain version (i.e. tests specify Latest, but we want to 
+            // say at the assembly-level that we end support at version X).
+            var maxVersion = testMethod.GetComputedMinProperty<string>(vsixAttribute, nameof(IVsixAttribute.MaximumVisualStudioVersion));
 
             var finalVersions = VsVersions.GetFinalVersions(vsVersions, minVersion, maxVersion);
 
@@ -77,6 +84,52 @@ namespace Xunit
             }
 
             return default(T);
+        }
+
+        public static T GetComputedMinProperty<T>(this ITestMethod testMethod, IAttributeInfo factAttribute, string argumentName)
+        {
+            var values = GetAggregatedProperties<T>(testMethod, factAttribute, argumentName);
+            values.Sort();
+
+            return values.FirstOrDefault();
+        }
+
+        public static T GetComputedMaxProperty<T>(this ITestMethod testMethod, IAttributeInfo factAttribute, string argumentName)
+        {
+            var values = GetAggregatedProperties<T>(testMethod, factAttribute, argumentName);
+            values.Sort();
+
+            return values.LastOrDefault();
+        }
+
+        private static List<T> GetAggregatedProperties<T>(this ITestMethod testMethod, IAttributeInfo factAttribute, string argumentName)
+        {
+            var values = new List<T>();
+            var value = factAttribute == null ? default(T) : factAttribute.GetNamedArgument<T>(argumentName);
+            if (!Object.Equals(value, default(T)))
+                values.Add(value);
+
+            // Go up to the class level.
+            var vsixAttr = testMethod.TestClass.Class.GetCustomAttributes(typeof(IVsixAttribute)).FirstOrDefault();
+            if (vsixAttr != null)
+            {
+                value = vsixAttr.GetNamedArgument<T>(argumentName);
+
+                if (!Object.Equals(value, default(T)))
+                    values.Add(value);
+            }
+
+            // Finally assembly level.
+            vsixAttr = testMethod.TestClass.Class.Assembly.GetCustomAttributes(typeof(IVsixAttribute)).FirstOrDefault();
+            if (vsixAttr != null)
+            {
+                value = vsixAttr.GetNamedArgument<T>(argumentName);
+
+                if (!Object.Equals(value, default(T)))
+                    values.Add(value);
+            }
+
+            return values;
         }
 
         public static T GetComputedArgument<T>(this ITestMethod testMethod, string argumentName)

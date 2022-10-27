552996834dc504c18d626f2b1980dfdbb94da111
Daniel Cazzulino <daniel@cazzulino.com>
2022-10-27 16:31:22 -0300

    Increase warmup time in our test run

--- src/Xunit.Vsix.Tests/AssemblyInfo.cs
+++ src/Xunit.Vsix.Tests/AssemblyInfo.cs
@@ -1,3 +1,5 @@
-﻿using Xunit;
+﻿using System.Diagnostics;
+using Xunit;
 
+[assembly: VsixRunner(WarmupMilliseconds = 10000, TraceLevel = SourceLevels.Information)]
 [assembly: Vsix(TimeoutSeconds = 90)]

f61c5cf0d09d35db124736bb6ae7742d7d078e16
Daniel Cazzulino <daniel@cazzulino.com>
2022-10-28 19:00:49 -0300

    Disconnect all remoting proxies before completion

---
+++ src/Xunit.Vsix/MarshalledObjects.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+
+namespace Xunit;
+
+class MarshalledObjects : IDisposable
+{
+    readonly List<MarshalByRefObject> marshalByRefs = new();
+
+    public T Add<T>(T mbr) where T : MarshalByRefObject
+    {
+        marshalByRefs.Add(mbr);
+        return mbr;
+    }
+
+    public void AddRange(IEnumerable<MarshalByRefObject> mbros)
+        => marshalByRefs.AddRange(mbros);
+
+    public void Dispose()
+    {
+        foreach (var mbro in marshalByRefs)
+            if (!RemotingServices.IsTransparentProxy(mbro))
+                RemotingServices.Disconnect(mbro);
+    }
+}

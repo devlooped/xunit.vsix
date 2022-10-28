9d0e65e90b8a307b71fa8e9ed639a7c6345b93ff
Daniel Cazzulino <daniel@cazzulino.com>
2022-10-28 18:32:37 -0300

    Avoid RPC errors for post-finish results collection
    The tests in the final event are remoted (potentially)
    and could thus not be available by the time the
    summary is collected. The vs-extensions-testing repo
    does this to avoid this issue and this might be the
    reason why we sometimes get RPC errors even after
    tests have successfully completed.

--- src/Xunit.Vsix/RemoteMessageBus.cs
+++ src/Xunit.Vsix/RemoteMessageBus.cs
@@ -1,24 +1,36 @@
-﻿using Xunit.Abstractions;
+﻿using System;
+using System.Linq;
+using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace Xunit
 {
     class RemoteMessageBus : LongLivedMarshalByRefObject, IMessageBus
     {
-        IMessageBus _localMessageBus;
+        readonly IMessageBus _localMessageBus;
 
-        public RemoteMessageBus(IMessageBus localMessageBus)
-        {
-            _localMessageBus = localMessageBus;
-        }
+        public RemoteMessageBus(IMessageBus localMessageBus) => _localMessageBus = localMessageBus;
 
-        public void Dispose()
-        {
-            _localMessageBus.Dispose();
-        }
+        public void Dispose() => _localMessageBus.Dispose();
 
+        [Obsolete]
         public bool QueueMessage(IMessageSinkMessage message)
         {
+            if (message is ITestAssemblyFinished testAssemblyFinished)
+            {
+                // The test cases in the ITestAssemblyFinished message are remote proxies, but the objects won't be
+                // used until after the remote process terminates. Recreate the objects in the current process to
+                // avoid using objects that are no longer available.
+                var testCases = testAssemblyFinished.TestCases.Select(testCase => testCase switch
+                {
+                    VsixTheoryTestCase theory => new VsixTestCase(new NullMessageSink(), TestMethodDisplay.ClassAndMethod, theory.TestMethod, theory.VisualStudioVersion, theory.RootSuffix, theory.NewIdeInstance, theory.Timeout, theory.RecycleOnFailure, theory.RunOnUIThread, theory.TestMethodArguments),
+                    VsixTestCase fact => new VsixTestCase(new NullMessageSink(), TestMethodDisplay.ClassAndMethod, fact.TestMethod, fact.VisualStudioVersion, fact.RootSuffix, fact.NewIdeInstance, fact.Timeout, fact.RecycleOnFailure, fact.RunOnUIThread, fact.TestMethodArguments),
+                    _ => new XunitTestCase(new NullMessageSink(), TestMethodDisplay.ClassAndMethod, TestMethodDisplayOptions.None, testCase.TestMethod, testCase.TestMethodArguments)
+                }); ;
+
+                message = new TestAssemblyFinished(testCases.ToArray(), testAssemblyFinished.TestAssembly, testAssemblyFinished.ExecutionTime, testAssemblyFinished.TestsRun, testAssemblyFinished.TestsFailed, testAssemblyFinished.TestsSkipped);
+            }
+
             return _localMessageBus.QueueMessage(message);
         }
     }
